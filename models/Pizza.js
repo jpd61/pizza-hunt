8d8161fc887e8076d32ca07bdd058a15ee79ac04
jpd61 <jpd@dewoodyoil.com>
2020-07-12 14:11:09 -0500

    validators

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,20 +3,26 @@ const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
+        type: String,
+        required: true,
+        trim: true
+      },
+      createdBy: {
+        type: String,
+        required: true,
+        trim: true
+      },
     createdAt: {
       type: Date,
       default: Date.now,
       get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     size: {
-      type: String,
-      default: 'Large'
-    },
+        type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        default: 'Large'
+      },
     toppings: [],
     comments: [
         {
